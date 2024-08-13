de39d97f40cc705410c399524c257c35e985594d
Alex Lustig <exoxander15@gmail.com>
2024-08-13 17:28:10 -0600

    working around complation issue by putting all definitions in main

--- OrbBox2/Main.cpp
+++ OrbBox2/Main.cpp
@@ -168,3 +168,17 @@ void PhysicsComponent::register_component() {
 		}
 	}
 }
+
+GameComponent* GameObject::get_component_by_name(const char* _name) {
+	std::vector<GameComponent*>::iterator it = object_components.begin();
+	std::vector<GameComponent*>::iterator end = object_components.end();
+	while (it != end) {
+		GameComponent* gc = *it;
+		if (gc->get_name() == _name) {
+			return gc;
+		}
+		std::advance(it, 1);
+	}
+
+	return nullptr;
+}
